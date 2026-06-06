cdcc1cab4504f326dd84b90f36c9e20807c6fb12
_stickman_ <danielsuarezbermudezfc@gmail.com>
2026-06-05 19:47:24 -0500

    tabla de rutas

--- entregas/daniel-suarez-proyecto06/app/frontend/src/app/app.routes.ts
+++ entregas/daniel-suarez-proyecto06/app/frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { VehiculosDentroComponent } from './pages/vehiculos-dentro/vehiculos-dentro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ruta vacía ('') -> redirige a /dentro como pantalla de inicio.
+  { path: '', redirectTo: 'dentro', pathMatch: 'full' },
+
+  // Cada objeto asocia una URL con el componente que se muestra en el <router-outlet>.
+  { path: 'dentro', component: VehiculosDentroComponent },
+
+  // (aquí iremos sumando: 'entrada', 'salida', 'tarifas', 'mensualidades', 'reportes'...)
+];
